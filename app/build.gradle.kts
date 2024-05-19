a852d5d4425499beae134942c8ba5cc2d312eb6c
Etrella39 <ju020807@naver.com>
2024-05-19 18:22:42 +0900

    Main button Edit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.recyclingapp"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -28,7 +28,13 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
