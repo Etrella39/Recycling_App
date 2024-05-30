b4a504b9386702773bdd3b553103b1b90e876eb5
Zhanyl3909 <dzh.2002z@gmail.com>
2024-05-30 13:11:00 +0900

    add Settings Back button features

--- app/src/main/java/com/example/recyclingapp/SetttigsActivity.kt
+++ app/src/main/java/com/example/recyclingapp/SetttigsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.recyclingapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Spinner
 
@@ -8,15 +9,19 @@ class SettingsActivity : AppCompatActivity() {
 
     private lateinit var screenModeSpinner: Spinner
     private lateinit var languageSpinner: Spinner
-
+    private lateinit var backButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_screen)
 
-
+        backButton = findViewById(R.id.back_button)
         screenModeSpinner = findViewById(R.id.my_spinner_screen_mode)
         languageSpinner = findViewById(R.id.my_spinner_language)
 
+
+        backButton.setOnClickListener {
+            finish()
+        }
 
     }
 }
