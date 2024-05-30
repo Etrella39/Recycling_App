4f4691c3522fb7a6eedc8b77a4b821ed6b8fa377
monesffff <ju020807@naver.com>
2024-05-30 15:13:20 +0900

    Edit setting screen

--- app/src/main/java/com/example/recyclingapp/SetttigsActivity.kt
+++ app/src/main/java/com/example/recyclingapp/SetttigsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.recyclingapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Spinner
 
@@ -9,14 +10,22 @@ class SettingsActivity : AppCompatActivity() {
     private lateinit var screenModeSpinner: Spinner
     private lateinit var languageSpinner: Spinner
 
+    private lateinit var backButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_screen)
 
 
-        screenModeSpinner = findViewById(R.id.my_spinner_screen_mode)
-        languageSpinner = findViewById(R.id.my_spinner_language)
+        backButton = findViewById(R.id.back_button)
 
+//        screenModeSpinner = findViewById(R.id.my_spinner_screen_mode)
+//        languageSpinner = findViewById(R.id.my_spinner_language)
+
+
+        backButton.setOnClickListener {
+            finish()
+        }
 
     }
 }
