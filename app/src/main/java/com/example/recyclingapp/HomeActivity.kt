10b04462071dda9ab78d2c224ef41a5ddfdf5777
Zhanyl3909 <dzh.2002z@gmail.com>
2024-05-30 09:35:17 +0900

    add SettingsActivity

--- app/src/main/java/com/example/recyclingapp/HomeActivity.kt
+++ app/src/main/java/com/example/recyclingapp/HomeActivity.kt
@@ -1,11 +1,26 @@
 package com.example.recyclingapp
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
-class HomeActivity : Activity() {
+class HomeActivity: Activity() {
+
+    private lateinit var setting_button : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_screen)
+
+
+        setting_button = findViewById(R.id.setting_button)
+
+        setting_button.setOnClickListener {
+            val intent = Intent(this@HomeActivity, SetttigsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/recyclingapp/SetttigsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.recyclingapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+
+class SetttigsActivity: Activity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_screen)
+
+
+    }
+
+    
+
+
+}
