22c772c35f3d24b17c555ce53c630635e207654c
Zhanyl3909 <dzh.2002z@gmail.com>
2024-06-02 02:17:55 +0900

    add User Profile

--- app/src/main/java/com/example/recyclingapp/HomeActivity.kt
+++ app/src/main/java/com/example/recyclingapp/HomeActivity.kt
@@ -6,19 +6,21 @@ import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var settingButton: Button
-
+    private lateinit var profile: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_screen)
 
         settingButton = findViewById(R.id.setting_button)
+        profile = findViewById(R.id.blogs_button)
 
 
         settingButton.setOnClickListener {
@@ -26,6 +28,10 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this@HomeActivity, SettingsActivity::class.java)
             startActivity(intent)
             overridePendingTransition(0, 0); // non animation
+        }
+        profile.setOnClickListener() {
+            val intent = Intent(this@HomeActivity, UserProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/recyclingapp/UserProfileActivity.kt
@@ -0,0 +1,24 @@
+package com.example.recyclingapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class UserProfileActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_profile)
+
+
+
+    }
+}
