d08d5601da8a95bddd72a6e1cd893ded2e090322
Zhanyl3909 <dzh.2002z@gmail.com>
2024-06-02 03:24:55 +0900

    add profile photo features

--- app/src/main/java/com/example/recyclingapp/UserProfileActivity.kt
+++ app/src/main/java/com/example/recyclingapp/UserProfileActivity.kt
@@ -2,39 +2,59 @@ package com.example.recyclingapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class UserProfileActivity : AppCompatActivity() {
 
+    private lateinit var user_main_photo: ImageView
 
-    private lateinit var user_photo : FrameLayout
+
     private lateinit var settingButton: Button
-
+    private lateinit var logout: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_profile)
 
-
-        user_photo = findViewById(R.id.user_photo_button)
+        user_main_photo = findViewById(R.id.profile_photo)
         settingButton = findViewById(R.id.setting_button)
+        logout = findViewById(R.id.log_out)
+//        userMainPhoto = findViewById(R.id.profile_photo)
 
-        user_photo.setOnClickListener() {
+        user_main_photo.setOnClickListener {
             val intent = Intent(this@UserProfileActivity, PhotoActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, REQUEST_CODE_SELECT_PHOTO)
         }
+
+
         settingButton.setOnClickListener {
-            Log.d("HomeActivity", "Setting button clicked")
             val intent = Intent(this@UserProfileActivity, SettingsActivity::class.java)
             startActivity(intent)
-
         }
 
+        logout.setOnClickListener {
+            val intent = Intent(this@UserProfileActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SELECT_PHOTO && resultCode == RESULT_OK) {
+            val selectedImageResourceId = data?.getIntExtra("selectedImageResourceId", -1)
+            if (selectedImageResourceId != null && selectedImageResourceId != -1) {
+                user_main_photo.setImageResource(selectedImageResourceId)
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE_SELECT_PHOTO = 1001
     }
 }
