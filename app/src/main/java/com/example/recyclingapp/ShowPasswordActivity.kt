142750cc2f84778e4e9b07737335a9e57279137d
Etrella39 <ju020807@naver.com>
2024-06-01 20:32:27 +0900

    change

--- app/src/main/java/com/example/recyclingapp/ShowPasswordActivity.kt
+++ app/src/main/java/com/example/recyclingapp/ShowPasswordActivity.kt
@@ -2,13 +2,15 @@ package com.example.recyclingapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ShowPasswordActivity : AppCompatActivity() {
 
-    private lateinit var okButton: Button
-    private lateinit var passwordTextView: TextView
+    private lateinit var okButton: RelativeLayout
+    private lateinit var passwordTextView: EditText
     private lateinit var dbHelper: DBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +18,7 @@ class ShowPasswordActivity : AppCompatActivity() {
         setContentView(R.layout.show_password_dialog)
 
         okButton = findViewById(R.id.find_button_for_show_dialog)
-        passwordTextView = findViewById(R.id.password_bo)
+        passwordTextView = findViewById(R.id.password_edit)
         dbHelper = DBHelper(this)
 
         val userId = intent.getStringExtra("USER_ID")
@@ -31,6 +33,6 @@ class ShowPasswordActivity : AppCompatActivity() {
 
     private fun showPassword(userId: String) {
         val password = dbHelper.findPasswordByUserId(userId)
-        passwordTextView.text = password ?: "Password not found"
+        passwordTextView.hint = password ?: "Password not found"
     }
 }
