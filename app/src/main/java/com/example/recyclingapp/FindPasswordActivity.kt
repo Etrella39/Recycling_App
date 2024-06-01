2f4c308b70d25ccb1b657ad26667fdde27c59122
Zhanyl3909 <dzh.2002z@gmail.com>
2024-06-01 19:42:04 +0900

    password update

--- app/src/main/java/com/example/recyclingapp/FindPasswordActivity.kt
+++ app/src/main/java/com/example/recyclingapp/FindPasswordActivity.kt
@@ -1,27 +1,25 @@
 package com.example.recyclingapp
 
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class FindPasswordActivity : AppCompatActivity() {
-  //  private lateinit var back_button : Button
+
+    private lateinit var back_button : Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.find_password_screen)
 
-       //back_button = findViewById(R.id.back_button)
+        back_button = findViewById(R.id.back_button)
 
-
-//        back_button.setOnClickListener {
-//            finish()
-//        }
+        back_button.setOnClickListener {
+            finish()
+        }
+        
     }
 
 }
