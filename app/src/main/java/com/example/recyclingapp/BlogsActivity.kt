b58f5925a70d2d877be3efb9250e608949850f14
Etrella39 <ju020807@naver.com>
2024-06-04 05:39:18 +0900

    change description

--- app/src/main/java/com/example/recyclingapp/BlogsActivity.kt
+++ app/src/main/java/com/example/recyclingapp/BlogsActivity.kt
@@ -90,27 +90,18 @@ class BlogsActivity : AppCompatActivity() {
     private fun getSampleBlogs(): List<Blog> {
         // Replace this with your actual data source (e.g., fetching from a server)
         val blogs = mutableListOf<Blog>()
-        blogs.add(Blog("New York Times", "There's an Explosion of Plastic Waste." +
-                "By 2025, Nestle promises not to use any plastic in its products that isn’t recyclable. " +
-                "By that same year, L’Oreal says all of its packaging will be “refillable, reusable, recyclable or compostable.”", "https://www.nytimes.com/2024/04/05/climate/plastic-recycling.html"))
+        blogs.add(Blog(getString(R.string.blog1_title), getString(R.string.blog1_description), "https://www.nytimes.com/2024/04/05/climate/plastic-recycling.html"))
 
-        blogs.add(Blog("Reduce, Reuse, Recycle", "The best place to start making a difference is right in your own home. " +
-                "Learn how you can reduce, reuse, and recycle materials to decrease household waste. The tips below will help you get started.", "https://www.epa.gov/recycle/reducing-waste-what-you-can-do"))
+        blogs.add(Blog(getString(R.string.blog2_title), getString(R.string.blog2_description), "https://www.epa.gov/recycle/reducing-waste-what-you-can-do"))
 
-        blogs.add(Blog("Inspiration: For Future Generation", "The ultimate test of man’s conscience may be his willingness to sacrifice something today " +
-                "for future generations whose words of thanks will not be heard.", "http://example.com/article3"))
+        blogs.add(Blog(getString(R.string.blog3_title), getString(R.string.blog3_description), "http://example.com/article3"))
 
-        blogs.add(Blog("Health and Climate", "Consumers rank malnutrition, pesticides, obesity and diet-related health issues, and plastic waste from packaging as the most " +
-                "concerning aspects of the food system, according to an EAT-GlobeScan survey.", "https://www.greenbiz.com/article/health-and-climate-top-consumers-concerns-about-food-system-report-finds"))
+        blogs.add(Blog(getString(R.string.blog4_title), getString(R.string.blog4_description), "https://www.greenbiz.com/article/health-and-climate-top-consumers-concerns-about-food-system-report-finds"))
 
-        blogs.add(Blog("Can You Recycle Underwear?", "When it comes to greening your wardrobe, a lot depends on what you do with your clothing at the end of its usefulness to you. " +
-                "We urge you to never send clothing to the landfill. Textile recycling is alive and well in the United States. Almost always, nonprofits that accept donations of clothing will recycle " +
-                "articles that are not reusable.\n", "https://earth911.com/how-to-recycle/recycling-mystery-underwear/"))
+        blogs.add(Blog(getString(R.string.blog5_title), getString(R.string.blog5_description), "https://earth911.com/how-to-recycle/recycling-mystery-underwear/"))
 
-        blogs.add(Blog("Preventing Wasted Food At Home", "Most people don't realize how much food they throw away every day — from uneaten " +
-                "leftovers to spoiled produce to parts of fruits and vegetables that could be eaten or repurposed.", "https://www.epa.gov/recycle/preventing-wasted-food-home"))
-        blogs.add(Blog("", "" +
-                "", ""))
+        blogs.add(Blog(getString(R.string.blog6_title), getString(R.string.blog6_description), "https://www.epa.gov/recycle/preventing-wasted-food-home"))
+        blogs.add(Blog("", "", ""))
         return blogs
 
     }
