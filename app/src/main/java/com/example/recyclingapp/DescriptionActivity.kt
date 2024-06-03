4a1a2b6de2ec16cfa4a70c4bce664f8838b2bb2b
Etrella39 <ju020807@naver.com>
2024-06-04 02:13:07 +0900

    change description

--- app/src/main/java/com/example/recyclingapp/DescriptionActivity.kt
+++ app/src/main/java/com/example/recyclingapp/DescriptionActivity.kt
@@ -2,17 +2,19 @@ package com.example.recyclingapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.ScrollView
-import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
 class DescriptionActivity : AppCompatActivity() {
     private lateinit var backButton: Button
     private lateinit var descriptionBack: ScrollView
 
+    private lateinit var parentView: RelativeLayout
+
+    private lateinit var layoutParams: RelativeLayout.LayoutParams
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,22 +22,42 @@ class DescriptionActivity : AppCompatActivity() {
 
         descriptionBack = findViewById(R.id.description_back)
 
+        parentView = findViewById(R.id.description_screen)
+
+        parentView.removeView(descriptionBack)
+
+
         val whatClicked = intent.getStringExtra("Description_KEY") ?: "paper"
         println(whatClicked)
 
-//        when (whatClicked) {
-//            "can" -> descriptionBack.set(R.layout.description_screen_paper)
-//            "glass" -> descriptionBack.setBackgroundResource(R.layout.description_screen_paper)
-//            "pet" -> descriptionBack.setBackgroundResource(R.layout.description_screen_paper)
-//            "vinyl" -> descriptionBack.setBackgroundResource(R.layout.description_screen_paper)
-//            "styro" -> descriptionBack.setBackgroundResource(R.layout.description_screen_paper)
-//            else -> descriptionBack.setBackgroundResource(R.layout.description_screen_paper)
-//        }
+        layoutParams = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.MATCH_PARENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT
+        )
+
+
+
+        when (whatClicked) {
+            "paper" -> setScrollScreen(R.layout.description_screen_paper)
+            "can" -> setScrollScreen(R.layout.description_screen_can)
+            "glass" -> setScrollScreen(R.layout.description_screen_glass)
+            "pet" -> setScrollScreen(R.layout.description_screen_pet)
+            "vinyl" -> setScrollScreen(R.layout.description_screen_vinyl)
+            "styro" -> setScrollScreen(R.layout.description_screen_styrofoam)
+            else -> setScrollScreen(R.layout.description_screen_paper)
+        }
 
         backButton = findViewById(R.id.back_button)
 
         backButton.setOnClickListener {
             finish()
         }
+    }
+
+    fun setScrollScreen(layout: Int) {
+        val newView = LayoutInflater.from(this).inflate(layout, parentView, false)
+        layoutParams.addRule(RelativeLayout.BELOW, R.id.back_button_2)
+        newView.layoutParams = layoutParams
+        parentView.addView(newView)
     }
 }
